685958884eb363cdea5525765148029c78a8c93e
nick thomas <nichoth@gmail.com>
2023-11-12 20:33:18 -0800

    wip

--- src/db.ts
+++ src/db.ts
@@ -8,8 +8,44 @@ export const serverID = 1
 async function initDB () {
     console.log('initializing database...')
     const db = newDb().adapters.createPgPromise()
+
+    await tx(async t => {
+        // A single global version number for the entire database.
+        await t.none(
+            'create table replicache_server (id integer primary key not null, version integer)',
+        )
+        await t.none(
+            'insert into replicache_server (id, version) values ($1, 1)',
+            serverID,
+        )
+
+        // Stores chat messages.
+        await t.none(`create table message (
+        id text primary key not null,
+        sender varchar(255) not null,
+        content text not null,
+        ord integer not null,
+        deleted boolean not null,
+        version integer not null)`)
+
+        // Stores last mutationID processed for each Replicache client.
+        await t.none(`create table replicache_client (
+        id varchar(36) primary key not null,
+        client_group_id varchar(36) not null,
+        last_mutation_id integer not null,
+        version integer not null)`)
+
+        // TODO: indexes
+    }, db)
+
     return db
 }
+
+// async function initDB () {
+//     console.log('initializing database...')
+//     const db = newDb().adapters.createPgPromise()
+//     return db
+// }
 
 function getDB () {
     // Cache the database in the Node global so that it survives HMR.
@@ -25,6 +61,7 @@ export async function tx<R> (
     dbp = getDB(),
 ) {
     const db = await dbp
+
     return await db.tx(
         {
             mode: new txMode.TransactionMode({
